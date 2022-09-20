78bb49b8488b8d33f3f6abb50ab3856a302153aa
2215778 <dlcoals0223@naver.com>
2022-09-20 23:16:00 +0900

    실습2(최종)

--- main.c
+++ main.c
@@ -4,16 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a, b;
+	char c;
+	int i;
 	
+	printf("input a number: ");
+	scanf("%c", &c);
 	
-	printf("분자를 입력하세요: ");
-	scanf("%d", &a);
+	i=c-'0';
 	
-	printf("분모를 입력하세요: "); 
-	scanf("%d", &b);
-	
-	printf("나누기의 결과는 %f 입니다.\n",(float)a/b);
+	printf("The input number is %i\n",i);
 	
 	return 0;
 }
